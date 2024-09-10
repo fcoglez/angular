a807b763b8f64fdf3ce792be57d11958a494c328
francisco gonzalez <franciscogonzalezhontoria@gmail.com>
2024-09-10 20:49:24 +0200

    project to test cli commands

---
+++ 01-clitest/src/app/pages/contac/contac.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contac',
+  standalone: true,
+  imports: [],
+  templateUrl: './contac.component.html',
+  styles: ``
+})
+export class ContacComponent {
+
+}
